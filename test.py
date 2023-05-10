022415a95203bacf91a830515ed6798414867d0c
Babu Izhan <babu.izhan@shopup.org>
2023-05-11 00:17:56 +0530

    update test cases2

--- test.py
+++ test.py
@@ -7,7 +7,3 @@ class TestSum(unittest.TestCase):
 
     def test_not_ai(self):
         self.assertEqual(ai(json.loads('{ "message": "This is not a math equation", "history" : [] }')), "Sorry, this is a invalid question. How can I help you with math?", "Should be a invalid math equation")
-
-    def test_ai_tuple(self):
-        self.assertIn("[4, 5, 7]",ai(json.loads('{ "message": "Add [[2,5,7], [4,7,9]] and [[2,0,0], [2,2,2]]", "history" : [] }')), "Should be able to solve the maths equation")
-        self.assertIn("[6, 9, 11]",ai(json.loads('{ "message": "Add [[2,5,7], [4,7,9]] and [[2,0,0], [2,2,2]]", "history" : [] }')), "Should be able to solve the maths equation")
